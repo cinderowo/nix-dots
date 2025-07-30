3a01481168f3899239cd4185385355d2bc63887c
charles <charlesohassellharris@gmail.com>
2025-07-30 13:51:39 +1000

    trying to add waybar

--- hm/home.nix
+++ hm/home.nix
@@ -8,6 +8,8 @@ in {
   imports = [
     ../stylix.nix
 
+    ../waybar/waybar.nix
+
     inputs.stylix.homeModules.stylix
     inputs.niri.homeModules.niri
   ];
@@ -29,6 +31,7 @@ in {
       gimp
       
       p7zip
+      unzip
       pavucontrol
       element-desktop
       nautilus
@@ -38,8 +41,17 @@ in {
       brightnessctl
       dconf
       git
+      zathura
+
+      dust
+      usbutils pciutils
+      toybox
+
+      xwayland-satellite
 
       ardour
+
+      corefonts
 
       inputs.zen-browser.packages."${system}".default
     ];
@@ -48,6 +60,9 @@ in {
 
   services = {
     hyprpaper = {
+      enable = true;
+    };
+    swaync = {
       enable = true;
     };
     # kanshi = {
@@ -93,9 +108,17 @@ in {
     fuzzel = {
       enable = true;
     };
+    fish = {
+      enable = true;
+    };
+    waybar = {
+      enable = true;
+      
+    };
     niri = {
       enable = true;
       settings.outputs = {
+        # left laptop monitor
         "eDP-1" = {
           scale = 1.0;
           mode = {
@@ -103,6 +126,7 @@ in {
             height = 1080;
           };
         };
+        # middle horizontal
         "DP-4" = {
           scale = 1.0;
           mode = {
@@ -115,6 +139,7 @@ in {
             y = 0;
           };
         };
+        # rightmost vertical
         "DP-7" = {
           scale = 1.0;
           transform.rotation = 90;
@@ -137,7 +162,10 @@ in {
         _JAVA_AWT_WM_NONREPARENTING="1";
       };
       
-      settings.binds = with config.lib.niri.actions; {
+      settings.binds = with config.lib.niri.actions;
+      let
+        fish = spawn "fish" "-c";
+      in {
         "${mod}+T".action.spawn = "${terminal}";
         "${mod}+D".action.spawn = "${menu}";
 
@@ -151,15 +179,33 @@ in {
 
         "${mod}+S".action = screenshot;
 
-        "${mod}+bracketright".action.spawn = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05+";
-        "${mod}+bracketleft".action.spawn = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05-";
-        "${mod}+KP_7".action.spawn = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
-        "${mod}+apostrophe".action.spawn = [ "brightnessctl set +10%" ];
-        "${mod}+semicolon".action.spawn = [ "brightnessctl set 10%-" ];
+        "${mod}+K".action = focus-window-or-workspace-up;
+        "${mod}+J".action = focus-window-or-workspace-down;
+        "${mod}+H".action = focus-column-or-monitor-left;
+        "${mod}+L".action = focus-column-or-monitor-right;
+
+        "${mod}+Shift+K".action = move-window-up-or-to-workspace-up;
+        "${mod}+Shift+J".action = move-window-down-or-to-workspace-down;
+        "${mod}+Shift+H".action = move-column-left-or-to-monitor-left;
+        "${mod}+Shift+L".action = move-column-right-or-to-monitor-right;
+
+        "Mod+Equal".action = set-column-width "+10%";
+        "Mod+Minus".action = set-column-width "-10%";
+        "Mod+Shift+1".action = set-column-width "50%";
+        "Mod+Shift+Equal".action = set-window-height "+10%";
+        "Mod+Shift+Minus".action = set-window-height "-10%";
+
+        "${mod}+Shift+O".action = fish "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05+";
+        "${mod}+Shift+P".action = fish "wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05-";
+        
+        "${mod}+Shift+U".action = fish "brightnessctl set +10%";
+        "${mod}+Shift+I".action = fish "brightnessctl set 10%-";
       };
 
       settings.spawn-at-startup = [
         { command = [ "hyprpaper" ]; }
+        { command = [ "xwayland-satellite" ]; }
+        { command = [ "waybar" ]; }
       ];
     };
   };
